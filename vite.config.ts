b61b691a49ba1ba28fae849d2d6cd1ff9b1aaf85
MondayNeurosis <49638789+hmsiegel@users.noreply.github.com>
2025-05-19 15:41:49 -0400

    Update TypeScript version and refactor Vite config for email theme building

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
 import tailwindcss from "@tailwindcss/vite"
+import { fileURLToPath } from "node:url";
+import path, { dirname, resolve } from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss(),
-        keycloakify({
-            accountThemeImplementation: "none",
-            postBuild: async (buildContext) => {
-                const { buildEmailTheme } = await import("keycloakify-emails");
-                const root = new URL(import.meta.url).pathname;
+export default defineConfig(({ command }) => {
+    const isBuild = command === "build";
+    return {
+        plugins: [
+            react(),
+            tailwindcss(),
+            keycloakify({
+                accountThemeImplementation: "none",
+                postBuild: async (buildContext) => {
+                    if (!isBuild) return;
+                    const { buildEmailTheme } = await import("keycloakify-emails");
+                    const root = process.cwd();
 
-                await buildEmailTheme({
-                    templatesSrcDirPath: `${root}/emails/templates`,
-                    i18nSourceFile: `${root}/emails/i18n.ts`,
-                    themeNames: buildContext.themeNames,
-                    keycloakifyBuildDirPath: buildContext.keycloakifyBuildDirPath,
-                    locales: ["en", "pl"],
-                    cwd: root
-                });
-            },
-        }),
-    ],
+                    await buildEmailTheme({
+                        templatesSrcDirPath: path.join(root, "emails", "templates"),
+                        i18nSourceFile: path.join(root, "emails", "i18n.ts"),
+                        themeNames: buildContext.themeNames,
+                        keycloakifyBuildDirPath: buildContext.keycloakifyBuildDirPath,
+                        locales: ["en", "pl"],
+                        cwd: root
+                    });
+                },
+            }),
+        ],
+    };
 });
